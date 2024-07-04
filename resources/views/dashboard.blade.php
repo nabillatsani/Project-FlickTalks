4f05d2d059f3374fb484ecef9f357f3464e18ea5
nabillatsani <150761988+nabillatsani@users.noreply.github.com>
2024-07-05 00:13:32 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,7 @@
     <title>Flicktalks Dashboard</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Gorga+Grotesque:wght@400;700&display=swap" rel="stylesheet">
     <style>
         body {
             background-color: #121212;
@@ -30,17 +31,23 @@
             padding: 30px;
             border-radius: 10px;
         }
-        h1 {
-            font-size: 5rem;
+        h3 {
+            font-size: 3rem;
             margin-bottom: 20px;
+            margin-top: -25px;
             color: #47a2ed;
         }
-        p {
+        h2{
+            color: #47a2ed;
+            margin-bottom: 30px;
+        }
+        .welcome {
             font-size: 1.25rem;
             color: #e0e0e0;
         }
         .card-deck {
-            margin-top: 20px;
+            margin-top: 30px;
+            margin-bottom: 30px;
             width: 100%;
             justify-content: center;
             gap: 20px;
@@ -52,10 +59,16 @@
             color: #e0e0e0;
             width: 180px;
             height: 250px;
+            transition: transform 0.2s;    
+        }
+        .card:hover {
+        transform: translateY(-20px);    
         }
         .card-title {
-            color: #47a2ed;
+            color: white;
+            text-decoration: none;
         }
+
         .move-up-down {
             display: inline-block;
             animation: moveUpDown 1s ease-in-out infinite alternate;
@@ -67,12 +80,30 @@
         }
         .box-content {
             background-color: #1f2937;
-            padding: 20px;
+            padding: 40px;
             border-radius: 10px;
             margin-top: 20px;
             width: 95%;
             margin-left: auto;
             margin-right: auto;
+        }
+        .diagram-container {
+            background-color: #1f2937;
+            padding: 20px;
+            border-radius: 10px;
+            margin-top: 20px;
+            width: 300px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin-top: 0px;
+            margin-bottom: 120px;
+            margin-left: 15px;
+            margin-right: 15px;
+        }
+        .diagram-wrapper {
+            display: flex;
+            justify-content: space-between; /* atau justify-content: space-around; */
         }
         .img-1{
             margin-top: 10px;
@@ -94,63 +125,114 @@
             margin-left: 10px;
             width: 160px;
         }
+        .count{
+            font-size: 8rem;
+            margin-bottom: -50px;
+            color: #47a2ed;
+            font-family: 'Gorga Grotesque', sans-serif;
+            font-weight: bold;
+        }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <div class="container">
         <div class="content">
-        <h1><span class="move-up-down">FlickTalks</span></h1>
-            <p>Welcome to the FlickTalks Staff page!<br>Please select the menu below to assist you with your work.</p>
+            <h3><span class="move-up-down">FlickTalks</span></h3>
+            <p class="welcome">Welcome {{ Auth::user()->name }}!</p>
         </div>
+        <div class="diagram-wrapper">
+            <div class="diagram-container">
+                <canvas id="myChart" width="300" height="300"></canvas>
+            </div>
+            <div class="diagram-container">
+            <canvas id="weeklyChart" width="300" height="300"></canvas>
+            </div>
+            <div class="diagram-container">
+                <p class="count">{{ $movieCount }}</p><br>
+                <p>Movies<br>Have Been Added!</p>
+            </div>
+        </div>            
+        
+        <h5>Select the menu below to help you in your work</h5>
+
         <div class="card-deck">
-        <a href="#section1">
-            <div class="card">
-            <img src="/storage/images/ToDo.PNG" class="card-img-top img-1" alt="Gambar 1">
-                <div class="card-body">
-                    <h5 class="card-title">What To Do?</h5>
+            <a href="#section1">
+                <div class="card">
+                    <img src="/storage/images/ToDo.PNG" class="card-img-top img-1" alt="Gambar 1">
+                    <div class="card-body">
+                        <h5 class="card-title">What To Do?</h5>
+                    </div>
                 </div>
-            </div></a>
+            </a>
             <a href="#section2">
-            <div class="card">
-                <img src="/storage/images/Manage.PNG" class="card-img-top img-2" alt="Gambar 2">
-                <div class="card-body">
-                    <h5 class="card-title">How to Manage Data</h5>
+                <div class="card">
+                    <img src="/storage/images/Manage.PNG" class="card-img-top img-2" alt="Gambar 2">
+                    <div class="card-body">
+                        <h5 class="card-title">How to Manage Data</h5>
+                    </div>
                 </div>
-            </div></a>
+            </a>
             <a href="#section3">
-            <div class="card">
-                <img src="/storage/images/Movie.PNG" class="card-img-top img-3" alt="Gambar 3">
-                <div class="card-body">
-                    <h5 class="card-title">Card 3</h5>
+                <div class="card">
+                    <img src="/storage/images/compolicy.PNG" class="card-img-top img-3" alt="Gambar 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Community Policy</h5>
+                    </div>
                 </div>
-            </div></a>
-        <a href="#section4">
-            <div class="card">
-                <img src="/storage/images/Company.PNG" class="card-img-top img-4" alt="Gambar 4">
-                <div class="card-body">
-                    <h5 class="card-title">About Flick Talks</h5>
+            </a>
+            <a href="#section4">
+                <div class="card">
+                    <img src="/storage/images/Company.PNG" class="card-img-top img-4" alt="Gambar 4">
+                    <div class="card-body">
+                        <h5 class="card-title">About Flick Talks</h5>
+                    </div>
                 </div>
-            </div></a>
+            </a>
         </div>
         <br><br><br><br><br>
         <div id="section1" class="box-content">
-            <h2>What To Do?</h2>
-            <p>Konten untuk section 1.</p>
+            <h2 style="text-align: left">What To Do?</h2>
+            <p style="text-align: left"> 1. Add a New Film to the Database<br>
+                2. Edit Film Data in Case of Data Changes<br>
+                3. Monitor Reviews Given by Users<br>
+                4. Delete Reviews if They Violate Rules for Everyone's Comfort</p><br>
+            <p style="text-align: left">Conditions for Deleting Reviews:<br>
+                1. If the review contains harsh language.<br>
+                2. If the review is completely unrelated to the film being reviewed.<br>
+                3. If the review touches on sensitive issues.</p>
         </div>
         <br><br><br><br><br>
         <div id="section2" class="box-content">
-            <h2>How to Manage Data?</h2>
-            <p>Konten untuk section 2.</p>
+            <h2 style="text-align: left">How to Manage Data?</h2>
+            <p style="text-align: justify; line-height: 2;" > 1. Data Entry : A brief guide to entering new data into the system with validation processes to ensure accuracy.<br>
+                2. Data Editing : Steps to edit or update existing data, including policies to maintain data consistency post-editing.<br>
+                3. Data Retrieval : Methods for retrieving data from the database for analysis or reporting using queries or appropriate search tools.<br>
+                4. Data Deletion : Criteria for deleting irrelevant or no longer needed data with procedures compliant with privacy and security policies.<br>
+                5. Data Backup and Recovery : Regular routines for creating data backups and procedures for data recovery in case of system loss or failure.<br>
+                6. Data Security : Data security policies covering access controls and encryption technologies to safeguard sensitive information.<br>
+                7. Data Maintenance : Routine processes to ensure data cleanliness and integrity, with team responsibilities for ongoing data quality maintenance.</p>
         </div>
         <br><br><br><br><br>
         <div id="section3" class="box-content">
-            <h2>sa</h2>
-            <p>Konten untuk section 3.</p>
+            <h2 style="text-align: left">Community Policy for Staff</h2>
+            <p style="text-align: justify; line-height: 2;"> 1. Professionalism : Act with a high level of professionalism in all interactions with team members and users. Respect the privacy and rights of others.<br>
+                2. Team Collaboration : Contribute positively to the team and support each other in achieving common goals. Communicate openly, honestly, and respect the perspectives of other team members.<br>
+                3. Data Security Policy : Adhere to the data security policy to protect user information and the interests of the platform. Take responsibility for managing and handling data according to established guidelines.<br>
+                4. Conflict Resolution : Handle conflicts wisely and professionally, seeking mutually beneficial solutions for all parties involved. Report issues or discomfort promptly and follow established procedures.<br>
+                5. Self-Development : Commit to learning and developing the necessary skills and knowledge. Utilize training and development opportunities to enhance performance and contribution.<br>
+                6. Policy Enforcement : Respect and enforce this community policy among staff members. Take consistent actions against policy violations according to established procedures.</p>
         </div>
         <br><br><br><br><br>
         <div id="section4" class="box-content">
-            <h2>About Flick Talks</h2>
-            <p>Konten untuk section 4.</p>
+            <h2 style="text-align: left">About Flick Talks</h2>
+            <p style="text-align: justify; line-height: 2;"> FlickTalks is an online platform dedicated to movie lovers throughout Indonesia. 
+                Here, you can find in-depth reviews of the latest movies, see opinions from different points of view, 
+                and contribute to your reviews. FlickTalks understands that watching movies is not just about entertainment, 
+                but is also an experience of sharing and discussing with a community that has the same interests. 
+                With our interactive features, you can explore movies, get recommendations, and connect with people who have 
+                similar interests in the world of movie. Join FlickTalks to make your movie watching experience more meaningful 
+                and connect with a dynamic global community.</p>
         </div>
         <br><br><br><br><br>
     </div>
@@ -158,6 +240,82 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <script>
+        // Data penjualan bulanan
+        var reviewData = {
+            Jan: 150,
+            Feb: 120,
+            Mar: 170,
+            Apr: 160,
+            May: 180,
+            Jun: 140,
+            Jul: 200,
+            Aug: 190,
+            Sep: 220,
+            Oct: 210,
+            Nov: 230,
+            Dec: 240
+        };
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                datasets: [{
+                    label: 'Data Review (Monthly)',
+                    data: [
+                        reviewData.Jan, reviewData.Feb, reviewData.Mar, reviewData.Apr,
+                        reviewData.May, reviewData.Jun, reviewData.Jul, reviewData.Aug,
+                        reviewData.Sep, reviewData.Oct, reviewData.Nov, reviewData.Dec
+                    ],
+                    backgroundColor: 'rgba(71, 162, 237, 0.3)',
+                    borderColor: 'rgba(2, 130, 235, 1)',
+                    borderWidth: 1,
+                    fill: true
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        // Data penjualan mingguan
+        var weeklySalesData = {
+            week1: 50,
+            week2: 75,
+            week3: 60,
+            week4: 90,
+        };
+
+        var ctxWeekly = document.getElementById('weeklyChart').getContext('2d');
+        var weeklyChart = new Chart(ctxWeekly, {
+            type: 'bar',
+            data: {
+                labels: ['Week 1', 'Week 2', 'Week 3', 'Week 4'],
+                datasets: [{
+                    label: 'Data Review (Weekly)',
+                    data: [
+                        weeklySalesData.week1, weeklySalesData.week2, weeklySalesData.week3, weeklySalesData.week4
+                    ],
+                    backgroundColor: 'rgba(71, 162, 237, 1)', // Biru muda transparan
+                    borderColor: 'rgba(71, 162, 237, 1)', // Biru
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
 </body>
 </html>
                         @elseif(Auth::user()->role === 'User')
@@ -342,7 +500,7 @@
         function showSlides(n) {
             slideIndex = (n + totalSlides) % totalSlides;
             const slidesContainer = document.querySelector('.slides');
-            slidesContainer.style.transform = `translateX(${-slideIndex * 100}%)`;
+            slidesContainer.style.transform = translateX(${-slideIndex * 100}%);
         }
 
         function plusSlides(n) {
